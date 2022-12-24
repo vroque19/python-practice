08d0693a40f619e14b15385797cdb6bbf29591b2
Vanessa <vroque19@csu.fullerton.edu>
2022-12-24 11:19:22 -0500

    "adding file"

---
+++ cs50/loops/forloops.py
@@ -0,0 +1,15 @@
+def main():
+    print_square(3)
+
+# def print_column(height):
+    # for _ in range(height):
+        # print("#")
+    #    print("#\n" * height, end = "")
+
+#def print_row(width):
+       # print("?" * width)
+
+def print_square(length):
+    for i in range(length):
+        print("#" *length)
+main()
