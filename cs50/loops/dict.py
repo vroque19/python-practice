c595739acfd5a597d29679a26b14137fa47e732d
Vanessa <vroque19@csu.fullerton.edu>
2022-12-23 22:36:21 -0500

    "dictionaries"

--- cs50/loops/dict.py
+++ cs50/loops/dict.py
@@ -1,11 +1,17 @@
 # key : owners = ["Ashley", "Mike", "Alex", "Rodney"]
 # value : dogs = ["Lily", "Bella", "Davey", "Lily", "Apollo"]
 
-dogs = {"Bella": "Mike",
+doggies = {"Bella": "Mike",
         "Lily": "Mike",
         "Lily": "Ashley",
         "Davey": "Alex",
         "Apollo": "Rodney"
         }
+
+dogs = [
+    {"name" : "Bella", "Breed" : "Silver Lab", "Age" : "11", "Owner" : "Mike"},
+    {"name" : "Lily", "Breed" : "Black Lab", "Age" : "6", "Owner" : "Ashley"},
+    {"name" : "Davey", "Breed" : "Golden Doodle", "Age" : "9", "Owner" : "Alex"},
+]
 for dog in dogs:
-    print(dog, dogs[dog], sep=", ")
+    print(dog["name"], dog["Age"], sep=", ")
