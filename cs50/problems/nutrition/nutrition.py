0eeb91822b220451e20cc82bf039aa457986cdad
Vanessa <vroque19@csu.fullerton.edu>
2022-12-27 00:24:40 -0500

    "moved things around"

--- cs50/problems/nutrition/nutrition.py
+++
@@ -1,9 +0,0 @@
-nutrition = {
-    "Apple" : "130",
-    "Sweet Cherries" : "100",
-    "Avacado" : "50",
-}
-
-fruit = input("Apple, Sweet Cherries, or Avacado?\n")
-
-print("Calories: " + nutrition[fruit.lower().capitalize()])
