f62488c60e0dfe061c4ead8e31f55baf5963a08e
Vanessa <vroque19@csu.fullerton.edu>
2022-12-28 21:13:14 -0500

    more reusable

--- exceptions/number2.py
+++ exceptions/number2.py
@@ -1,13 +1,13 @@
 def main():
-    x = get_int()
+    x = get_int("What is x? ")
     print(f"x is {x}")
 
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            x = int(input("What is x? "))
-            return x
+            return int(input(prompt))
         except ValueError:
             print("x is not an integer")
+            # pass
 
 main()
